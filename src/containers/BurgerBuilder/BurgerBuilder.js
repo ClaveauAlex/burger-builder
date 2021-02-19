40417278009e5e5d741e2d8e60ada669e9c82c18
Alexandre Claveau <a.claveau29@gmail.com>
2021-02-19 16:18:30 +0100

    Displaying and updating the Burger Price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -64,6 +64,7 @@ class BurgerBuilder extends Component{
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disableInfos}
+                    price={this.state.totalPrice}
                 />
             </Aux>
         );
